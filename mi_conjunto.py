e980999a72cefc884fe6784375d5de126671df80
AndresRivera111 <a.22308051281295@cbtis128.edu.mx>
2024-09-11 07:37:07 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de conjuntos")
+tipodeportes={"Futbol","Tenis","Basquetbol",}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+for rivera in tipodeportes:
+  print(rivera)
